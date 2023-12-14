b271217e7fb4ac66b640ebdf535356c18a8858a0
mdakbari <manthanakbari999@gmail.com>
2023-12-14 12:16:49 +0530

    14-12-23

---
+++ _next/static/chunks/pages/blog/page/[page]-ae91ee946e6cebe6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[51],{1125:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/page/[page]",function(){return u(6699)}])}},function(n){n.O(0,[802,310,790,62,699,774,888,179],function(){return n(n.s=1125)}),_N_E=n.O()}]);
